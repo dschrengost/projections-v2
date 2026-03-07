49cca1ca684f1823a20565f7eb6d919cf4294f95
Dschrengost <dschrengost@gmail.com>
2026-03-07 10:27:34 -0500

    optimizer: fix lineups section closing tag

--- web/minutes-dashboard/src/pages/OptimizerPage.tsx
+++ web/minutes-dashboard/src/pages/OptimizerPage.tsx
@@ -2075,7 +2075,7 @@ export default function OptimizerPage() {
                                     </button>
                                 </div>
                             )}
-                        </div>
+                        </section>
                     )}
                 </div>
             </div>
